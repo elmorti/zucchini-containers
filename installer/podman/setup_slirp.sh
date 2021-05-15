0c5b6444d8d5f122f651e596d309aae3c26a33ac
Samuelito <samuel.partida@gmail.com>
2021-05-15 17:50:32 +0000

    Podman testing and automation

--- installer/podman/setup_slirp.sh
+++
@@ -1,1 +0,0 @@
-slirp4netns --configure --mtu=65520 --api-socket /tmp/slirp4netns.sock --disable-host-loopback $(cat /tmp/pid) tap0
--- installer/podman/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-podman run --rm \
-  --name dnsmasq \
-  -it \
-  -v $(pwd)/config/dnsmasq.conf:/etc/dnsmasq.conf:Z \
-  -v $(pwd)/config/dnsmasq.d:/etc/dnsmasq.d:Z \
-  -p 53:53/udp -p 53:53/tcp \
-  zucchini-dns:dev
