60a4876e095cc944cf2f42efd16dc329e8d0342b
Samuelito <samuel.partida@gmail.com>
2023-07-16 17:47:50 +0100

    Update git

--- git/podman_git.sh
+++ git/podman_git.sh
@@ -5,7 +5,9 @@ VERSION="latest"
 
 # man podman-run to customize this - label=disable is fine as we want
 # to run "git" as a command, so it won't be confined as a container_t
+# this is intended to mount home as /root
 SECURITY_OPT="--security-opt label=disable"                                                                 
+EXPOSE_SSH_AGENT="-v $(readlink -f $SSH_AUTH_SOCK):/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
 
 PODMAN=$(whereis podman | cut -d' ' -f2)
 if [ ! -x ${PODMAN} ]
@@ -14,4 +16,5 @@ then
   exit 1
 fi
 
-${PODMAN} run ${SECURITY_OPT} -it --rm -v ${HOME}:/root -v $(pwd):/workdir ${IMAGE}:${VERSION} "$@"
+# ${PODMAN} run ${SECURITY_OPT} -it --rm ${EXPOSE_SSH_AGENT} -v ${HOME}:/root -v $(pwd):/workdir ${IMAGE}:${VERSION} "$@"
+echo "${PODMAN} run ${SECURITY_OPT} -it --rm ${EXPOSE_SSH_AGENT} -v ${HOME}:/root -v $(pwd):/workdir ${IMAGE}:${VERSION}"
